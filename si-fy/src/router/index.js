64c88e242a1e808b01e7dc471100a80839c846f0
Bruno Feijó Rocha <42208856+feijobruno@users.noreply.github.com>
2020-03-26 16:29:03 -0300

    Add files via upload
    Routes refatorado.

--- si-fy/src/router/index.js
+++ si-fy/src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Playlist from "../views/Playlist.vue";
+import Login from "../views/Login.vue";
+import Callback from "../views/Callback.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  }
-];
-
-const router = new VueRouter({
-  routes
-});
-
-export default router;
+export default new VueRouter({
+  mode: 'history',
+  // mode: 'hash',
+  routes: [
+      { path: '/', name : "home" , component: Home },
+      { path: '/playlist', name : "playlist", component: Playlist },
+      { path: '/login', name : "Login", component: Login },
+      { path: '/callback', name : "Callback", component: Callback }
+  ]
+})
