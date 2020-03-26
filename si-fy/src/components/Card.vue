3807dc1a09578a41f61d22e1e6cfabe47ae6bb82
Bruno Feijó Rocha <42208856+feijobruno@users.noreply.github.com>
2020-03-26 16:31:42 -0300

    Add files via upload

---
+++ si-fy/src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid>
+    <v-card id="card-item" color="#4E4E4E" outlined tile>
+      <v-img height="90%" width="100%" :src="image"></v-img>
+      <v-card-subtitle id="card-title" class="headline grey--text text--lighten-2" v-text="title" />
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ["title", "image"]
+};
+</script>
+
+<style scoped>
+#card-item {
+  height: 210px;
+  width: 200px;
+}
+
+#card-title {
+  margin: 0px;
+  padding: 0px;
+  text-align: center;
+}
+</style>
--- si-fy/src/components/Sidebar.vue
+++ si-fy/src/components/Sidebar.vue
@@ -7,25 +7,23 @@
     color="#404040"
     padless
   >
+  <router-link to="/">
     <v-img src="https://randomuser.me/api/portraits/women/85.jpg">
       <v-list>
         <v-list-item link>
-          <v-list-item-content>
+         <v-list-item-content>
             <v-list-item-title class="title">Sandra Adams</v-list-item-title>
             <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-img>
-
+ </router-link>
     <v-divider></v-divider>
-
-    <v-list nav dense>
+    <v-list nav dense > 
       <v-list-item link v-for="item in list_items" :key="item">
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>{{ item.text }}</v-list-item-title>
+          <router-link :to="item.to"><v-list-item-title><v-icon>{{ item.icon }}</v-icon>{{ item.text }}</v-list-item-title>
+        </router-link>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
@@ -36,17 +34,25 @@ export default {
   data() {
     return {
       list_items: [
-        { icon: "music_note", text: "Songs" },
-        { icon: "music_note", text: "Playlists" },
-        { icon: "music_note", text: "Artists" },
-        { icon: "music_note", text: "Albums" },
-        { icon: "music_note", text: "Podcasts" },
-        { icon: "logout", text: "Logout" }
+        { icon: "music_note", text: "Playlists", to: '/playlist' },
+        { icon: "music_note", text: "Songs", to:'/songs' },
+        { icon: "music_note", text: "Artists", to: '/artist' },
+        { icon: "music_note", text: "Albums", to: '/album' },
+        { icon: "music_note", text: "Podcasts", to: '/podcast' }, 
+        { icon: "logout", text: "Logout", to: '/logout' }
       ]
     };
   },
-  methods: {}
+  methods: {
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+a {
+  text-decoration: none;
+}
+a .v-list-item__title {
+  color: whitesmoke;
+}
+</style>
